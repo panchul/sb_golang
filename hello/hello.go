83e1a9334b0224d3ae2ed1067ee8e5fcfb94448d
Aleksandr Panchul <apanchul@hotmail.com>
2016-10-30 23:28:55 -0400

    adding simple app, package, and a test

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "github.com/vagrant/sb_golang/stringutil"
+)
+
+func main() {
+    fmt.Printf("Hello, world!\n")
+    fmt.Println(stringutil.Reverse("Hello, world!"))
+}
+
